d10efb7b11ec89c0ad2ffde038aad39e3f215210
pramudyanuar <pramudyanuar@gmail.com>
2024-02-26 23:30:02 +0700

    add pertemuan 1 not final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import CreateTask from './components/createTask';
+import ListTask from './components/taskList';
+import {Toaster} from 'react-hot-toast';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import Navbar from './components/Navbar';
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  console.log("tasks", tasks);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='pb-[76px] flex flex-col'>
+        <div className='-z-10'><Navbar /></div>
+        <div className='pt-3 pr-4'><CreateTask tasks={tasks} setTasks={setTasks}/></div>
+      </div>
+      <div>
+        <DndProvider backend={HTML5Backend}>
+          <Toaster /> 
+          <div className= "flex flex-col items-center p-3 gap-16">
+            <ListTask tasks={tasks} setTasks={setTasks}/>
+          </div>
+        </DndProvider>
+      </div>
+    </>
   );
 }
 
